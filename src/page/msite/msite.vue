01ef2b0737b8d786834f24ea0da77a7a6d415b4c
zhangjingjing <zhangjingjing@viewhigh.com>
2019-12-05 17:28:56 +0800

    swiper轮播图

--- src/page/msite/msite.vue
+++ src/page/msite/msite.vue
@@ -9,6 +9,22 @@
             {{this.cityAll.address}}
         </router-link>
     </header-top>
+    <div class="swiper-container">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+    
+    <!-- 如果需要导航按钮 -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    
+    <!-- 如果需要滚动条 -->
+    <div class="swiper-scrollbar"></div>
+</div>
      <footer-guide></footer-guide>
 </div>
 </template>
@@ -18,7 +34,8 @@ import  header from "components/header/header"
 import  footerGuide from "components/footer/footerGuide"
 import {getCityAll,getIndexEntry} from "service/getData"
 import log from '../../../node_modules_/loglevel';
-import "../../plugins/swiper.min.js";
+// import "../../plugins/swiper.min.js";
+import Swiper from "swiper";
 import "../../style/swiper.min.css"
 import {mapState} from "vuex";
 export default {
@@ -50,7 +67,26 @@ created() {
     this.getIndexEntry();
 },
 mounted() {
-
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    // 如果需要滚动条
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })  
 },
 components:{
     headerTop:header,
@@ -61,6 +97,10 @@ components:{
 <style lang='scss' scoped>
 @import "style/scss.scss";
 // @import "style/swiper.scss";
+.swiper-container {
+    width: 600px;
+    height: 300px;
+}  
 .search{
      @include atcenter;
       left:0.5rem;
